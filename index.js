29631eda1553ac63808652c570b08e2c3faa9928
Felippe Chemello <felippechemello@gmail.com>
2020-12-03 11:27:17 -0300

    First Commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const cors = require('cors')
+
+const port = process.env.PORT || 3333
+
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+
+app.get('/', (request, response) => {
+    response.send('GET na raiz do site')
+})
+
+app.listen(port, () => {
+    console.log(`Server started on port ${port}`)
+})
