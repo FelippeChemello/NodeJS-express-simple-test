09bd941b65b4b7eadaec6e75af6792d67c3a45dd
Felippe Chemello <felippechemello@gmail.com>
2020-12-03 11:43:18 -0300

    Hook

---
+++ post-receive.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+echo "[post-receive] Iniciando Hook"
+
+echo "[post-receive] Criando Diretório do Repositório"
+mkdir -p /root/NodeJS-express-example-deploy
+
+echo "[post-receive] Acessando Diretório do Repositório"
+cd /root/NodeJS-express-example-deploy
+
+echo "[post-receive] Git Checkout"
+git --git-dir=/root/NodeJS-express-simple-test.git --work-tree=/root/NodeJS-express-example-deploy checkout master -f
+
+echo "[post-receive] Docker-Compose down"
+docker-compose down &&
+
+echo "[post-receive] Docker-Compose up"
+docker-compose up
